873aba231e7f5e65579242e7cf4426d690ba77f6
BC-CesarGutierrez <cesar.gutierrez@bigcommerce.com>
2019-05-08 15:56:53 -0500

    feat(checkout): INT-1503 Mocks updated

--- src/discount/discounts.mock.ts
+++ src/discount/discounts.mock.ts
@@ -4,5 +4,6 @@ export function getDiscount(): Discount {
     return {
         id: '12e11c8f-7dce-4da3-9413-b649533f8bad',
         discountedAmount: 10,
+        discountedAmountAsInteger: 10,
     };
 }
