27d4faefd7c09d7d8d5bf0f4b9f6f2c639ad5cf0
AlanMunozN <amunoz@intersysconsulting.com>
2019-12-19 16:13:25 -0600

    feat(payment): INT-1997 Test

--- dist/checkout-sdk.d.ts
+++ dist/checkout-sdk.d.ts
@@ -295,6 +295,34 @@ declare interface BlockElementStyles extends InlineElementStyles {
     borderWidth?: string;
 }
 
+/**
+ * A set of options that are required to initialize the BlueSnap V2 payment
+ * method.
+ *
+ * The payment step is done through a web page via an iframe provided by the
+ * strategy.
+ */
+declare interface BlueSnapV2PaymentInitializeOptions {
+    /**
+     * A callback that gets called when the iframe is ready to be added to the
+     * current page. It is responsible for determining where the iframe should
+     * be inserted in the DOM.
+     *
+     * @param error - Any error raised during the payment process;
+     * undefined if there is none.
+     * @param iframe - The iframe element containing the payment web page
+     * provided by the strategy.
+     * @param cancel - A function, when called, will cancel the payment
+     * process and remove the iframe.
+     */
+    addFrame(error: Error | undefined, iframe: HTMLIFrameElement, cancel: () => void): void;
+    /**
+     * A callback that gets called when the iframe is about to be removed from
+     * the current page.
+     */
+    removeFrame(): void;
+}
+
 declare interface BodyStyles {
     backgroundColor?: string;
 }
@@ -3080,6 +3108,11 @@ declare interface PaymentInitializeOptions extends PaymentRequestOptions {
      * method. They can be omitted unless you need to support AmazonPay.
      */
     amazon?: AmazonPayPaymentInitializeOptions;
+    /**
+     * The options that are required to initialize the BlueSnapV2 payment method.
+     * They can be omitted unless you need to support BlueSnapV2.
+     */
+    bluesnapv2?: BlueSnapV2PaymentInitializeOptions;
     /**
      * The options that are required to initialize the Braintree payment method.
      * They can be omitted unless you need to support Braintree.
